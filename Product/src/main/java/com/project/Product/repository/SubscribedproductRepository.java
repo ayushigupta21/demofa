4840bf54409d1834e327d91d538c2f9542f3d68a
ayushi <ayushigupta4632@gmail.com>
2021-03-10 09:46:51 +0530

    updated product

---
+++ Product/src/main/java/com/project/Product/repository/SubscribedproductRepository.java
@@ -0,0 +1,9 @@
+package com.project.Product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.Product.entity.Subscribedproduct;
+
+public interface SubscribedproductRepository extends JpaRepository<Subscribedproduct, Integer>{
+
+}
